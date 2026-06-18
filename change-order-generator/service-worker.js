bbb8d91c5989d87ab5ebf2849f86c8f68e55a9e9
XxEagleFangxX <raysallisky@gmail.com>
2026-06-18 10:53:27 -0500

    App: Report-a-Bug email to hello@digitalforemansuite.com + SW cache-v14

--- change-order-generator/service-worker.js
+++ change-order-generator/service-worker.js
@@ -24,7 +24,7 @@
       the rest, and failures are logged to the console.
    =========================================================*/
 
-const CACHE_NAME = "change-order-cache-v13";
+const CACHE_NAME = "change-order-cache-v14";
 
 /* Files served from our own site. */
 const APP_SHELL = [
