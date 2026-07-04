bc469cbe168e1592a8d2322abf0cdfa5910fd8b4
XxEagleFangxX <raysallisky@gmail.com>
2026-07-04 15:21:38 -0500

    Bid Leveler: group flags per bidder on busy jobs (3+ under-lines collapse to one summary); SW v6

--- bid-leveler/service-worker.js
+++ bid-leveler/service-worker.js
@@ -5,7 +5,7 @@
    - individual fetch per asset so one bad path can't kill offline
    ========================================================= */
 
-const CACHE_NAME = "bid-leveler-cache-v5";
+const CACHE_NAME = "bid-leveler-cache-v6";
 
 const APP_SHELL = [
   "./",
