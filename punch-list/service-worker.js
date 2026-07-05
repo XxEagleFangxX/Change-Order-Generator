87c2e4156d97b2bcc8bca7bad61cf1e3ed583330
XxEagleFangxX <raysallisky@gmail.com>
2026-07-05 15:48:25 -0500

    PL: company name in PDF header (Pro) + install card iOS/no-prompt fallbacks; SW v9

--- punch-list/service-worker.js
+++ punch-list/service-worker.js
@@ -5,7 +5,7 @@
    - individual fetch per asset so one bad path can't kill offline
    ========================================================= */
 
-const CACHE_NAME = "punch-list-cache-v8";
+const CACHE_NAME = "punch-list-cache-v9";
 
 const APP_SHELL = [
   "./",
