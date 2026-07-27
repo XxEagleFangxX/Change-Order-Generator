9d97a5c2b1701012d184e1d24cccfba5a108222c
XxEagleFangxX <raysallisky@gmail.com>
2026-07-26 23:44:48 -0500

    BL letterhead header per spec: company name is the star (matches CO) + SW v17

--- bid-leveler/service-worker.js
+++ bid-leveler/service-worker.js
@@ -5,7 +5,7 @@
    - individual fetch per asset so one bad path can't kill offline
    ========================================================= */
 
-const CACHE_NAME = "bid-leveler-cache-v16";
+const CACHE_NAME = "bid-leveler-cache-v17";
 
 const APP_SHELL = [
   "./",
